1718314306e15f9d9b1625140623ef115000a526
Bryan <bryanbuitrago86@gmail.com>
2020-06-30 15:11:15 -0700

    Updates

---
+++ precache-manifest.eadad81d558b517c81e42e2a530e5c93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61e1a373b327185bc927",
+    "url": "/static/js/main.61e1a373.chunk.js"
+  },
+  {
+    "revision": "2280db13f951d6abd8f1",
+    "url": "/static/js/1.2280db13.chunk.js"
+  },
+  {
+    "revision": "61e1a373b327185bc927",
+    "url": "/static/css/main.192d0968.chunk.css"
+  },
+  {
+    "revision": "46ea3d44cc9f9b0518d1187b26460d52",
+    "url": "/index.html"
+  }
+];
